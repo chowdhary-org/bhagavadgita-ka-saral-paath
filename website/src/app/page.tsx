9f9843ef32c2db5ac84f2d6a78cebb82351a09cb
Anand Chowdhary <mail@anandchowdhary.com>
2025-11-24 10:51:56 +0100

    :tada: Add website

---
+++ website/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Author } from '@/components/Author'
+import { Footer } from '@/components/Footer'
+import { Hero } from '@/components/Hero'
+import { Introduction } from '@/components/Introduction'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Introduction />
+      <Author />
+      <Footer />
+    </>
+  )
+}
